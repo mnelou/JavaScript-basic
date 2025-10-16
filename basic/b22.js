b6aace83fc03bc1496b76db9066cbbcaf786df4f
Muhammad Nauman <ah993972@gmail.com>
2025-10-16 18:12:42 +0000

    switch case

---
+++ basic/b22.js
@@ -0,0 +1,32 @@
+//switch case before we covers if else etc
+//syntax of switch case 
+// switch(key){
+//     case value:
+//         break;
+
+//         case value2:
+//             break;
+
+//             defualt:
+//             break;
+// }
+
+
+const month=3
+
+switch(month){
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tueday");
+        break;
+        
+    case 3:
+        console.log("Friday");
+        break;
+
+        default:
+            break;
+
+}
