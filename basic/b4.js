e4f7b9246fc6b674e4f9475b9fc761b3a221faa9
Muhammad Nauman <ah993972@gmail.com>
2025-10-16 17:35:38 +0000

    operation on operator in different way

---
+++ basic/b4.js
@@ -0,0 +1,49 @@
+// // operations in javascript 
+// // for assign change
+// let value=3
+// let negvalue=-value
+
+// console.log(negvalue);
+// // it print -3 value
+
+// // some arithmatic operation in print structure
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3%3);
+// console.log(3/3);
+
+// // how to combine string
+// let str1="hello"
+// let str2=" hitesh"
+// let str3=str1+str2
+
+// console.log(str3)
+
+// console.log("1"+2);// print 12
+// console.log(1+"2");// print 12
+// //they print 12
+
+// //for the sum of two string
+// console.log("1"+"2");// print 12
+// console.log("2"+1+3); // print 213
+// console.log(1+3+"2");// print 42
+
+
+
+
+// console.log(true);//print true
+// console.log(2+true);//print 3
+// console.log(true+2);//print 3
+
+// // these operation are valid but here is not use readability and  short of code
+// let num1,num3,num4
+// num1=num3=num4=4+3
+
+//increament operator
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter);// it print 101
+ /*difference between pre and post increament operator is only in in pre => increament 
+ occur instantly in post =>increament occur after assign the operator and then in
+  next itration it will increase
+*/
