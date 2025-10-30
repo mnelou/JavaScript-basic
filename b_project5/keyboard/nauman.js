af5a36111c034e9efa334f295661eb4d65fbf0d0
Muhammad Nauman <ah993972@gmail.com>
2025-10-30 09:14:33 +0000

    show press keyboard value

---
+++ b_project5/keyboard/nauman.js
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="../styles.css" />
+    <title>JavaScript Background Color Switcher</title>
+  </head>
+  <style>
+table,th,td{
+  border:1px solid #e7e7e7;
+}
+.project{
+  background-color:#1c1c1c;
+  color:#ffffff;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  text-align:center;
+  height:100vh;
+}
+.color{
+  color:aliceblue;
+  display:flex;
+  flex-direction:row;
+}
+</style>
+  <body>
+    <nav>
+      <a href="/" aria-current="page">Home</a>
+      <a target="_blank" href="https://www.youtube.com/@chaiaurcode"
+        >Youtube channel</a
+      >
+    </nav>
+    <div class="project">
+      <div id="insert">
+        <div class="key">press teh key watch magic</div>
+      </div></div>
+
+      <script src="./chaiaurcode.js">
+
+      </script>
+  </body>
+</html>
